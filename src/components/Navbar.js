a61cad6528a05fdf8c76a9c497b3d632dcdecd9a
Vinayak Sawant <131691007+vinayaks02@users.noreply.github.com>
2023-09-01 16:20:47 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Navbar, Nav } from "react-bootstrap";
 
 const AppNavbar = () => {
+  const [expanded, setExpanded] = useState(false);
+
+  const closeNav = () => {
+    setExpanded(false);
+  };
+
   return (
-    <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect fixed="top">
-      <Navbar.Brand as={Link} to="/">
+    <Navbar
+      bg="dark"
+      variant="dark"
+      expand="md"
+      expanded={expanded}
+      fixed="top"
+      collapseOnSelect // Add this prop
+    >
+      <Navbar.Brand as={Link} to="/" onClick={closeNav}>
         QuizzerZest
       </Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav" className="ml-auto">
+      <Navbar.Toggle
+        aria-controls="responsive-navbar-nav"
+        onClick={() => setExpanded(!expanded)}
+      />
+      <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="justify-content-end">
-          <Nav.Link as={Link} to="/" style={{ color: 'yellow' }}>
+          <Nav.Link as={Link} to="/" onClick={closeNav} style={{ color: "yellow" }}>
             Home
           </Nav.Link>
-          <Nav.Link as={Link} to="/create" style={{ color: 'yellow' }}>
-            Create Quiz 
+          <Nav.Link as={Link} to="/create" onClick={closeNav} style={{ color: "yellow" }}>
+            Create Quiz
           </Nav.Link>
-          <Nav.Link as={Link} to="/edit" style={{ color: 'yellow' }}>
+          <Nav.Link as={Link} to="/edit" onClick={closeNav} style={{ color: "yellow" }}>
             Edit Quiz
           </Nav.Link>
-          <Nav.Link as={Link} to="/leaderboard" style={{ color: 'yellow' }}>
+          <Nav.Link as={Link} to="/leaderboard" onClick={closeNav} style={{ color: "yellow" }}>
             Leaderboard
           </Nav.Link>
         </Nav>
